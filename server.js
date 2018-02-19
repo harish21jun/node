acd1cd23447c6d2706ee6adea6c7a5dd48723a89
harish21jun <33644132+harish21jun@users.noreply.github.com>
2018-02-18 16:23:35 -0800

    Changed based on generic server.js file

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.delete('/posts/:postId/comments/:commentId',routes.comments.removeComment)
 
 //app.listen(2000)
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-app.listen(server_port, server_ip_address, function () {
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+app.listen(port, ip, function () {
     console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
 });
